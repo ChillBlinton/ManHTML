8d794cd5f50150ae3aaeeadd9e9f65e7d91948e1
CB <wtbatson@gmail.com>
2022-02-21 10:00:06 -0600

    Lost of iffy changes

--- DOM/app.js
+++ DOM/app.js
@@ -1,1 +1,72 @@
-ECHO is on.
+document.addEventListener('DOMContentLoaded', function (event) {
+
+    // Nice Message
+    let btn = document.createElement('button')
+    btn.className = 'niceButton'
+    let btnText = document.createTextNode('DomButton')
+    btn.appendChild(btnText)
+    document.body.appendChild(btn)
+
+    btn.addEventListener('click', function () {
+        alert('Nice Message Brah')
+    });
+
+
+    let htmlButton = document.getElementById('htmlButton');
+    htmlButton.addEventListener('click', function () {
+
+        alert(document.getElementById('textBox').value);
+
+
+    });
+
+    let colorDiv = document.getElementById('div1')
+    colorDiv.addEventListener('mouseenter', function () {
+        colorDiv.style.backgroundColor = 'red';
+    })
+
+    colorDiv.addEventListener('mouseleave', function () {
+        colorDiv.style.backgroundColor = 'white';
+
+    });
+
+    let p = document.createElement('p')
+    let pTxt = document.createTextNode('Paragraphing out')
+    p.appendChild(pTxt)
+    document.body.appendChild(p)
+
+    p.addEventListener('click', function () {
+        let colorChange = ['Green', 'Pink', 'Yellow', 'blue', 'black', 'brown', 'purple', 'Red'];
+        let randomIndex = Math.floor(Math.random() * colorChange.length)
+        p.style.color = colorChange[randomIndex];
+        p.style.cursor = 'pointer';
+
+    });
+
+    // Add name when click
+    let emptyDiv = document.getElementById('emptyDiv')
+    let emptyBtn = document.getElementById('emptyDivBtn')
+    emptyBtn.addEventListener('click', function () {
+        let newSpan = document.createElement('span')
+        emptyDiv.appendChild(newSpan)
+        let newSpanTxt = document.createTextNode('Yung Billiam')
+        newSpan.appendChild(newSpanTxt)
+    });
+
+    // Add friendos 
+    let friends = ['Chungus', 'Fungus', 'Amongus', 'Bruhngus', 'Jeb']
+    let lastBtn = document.getElementById('lastBtn');
+    let ul = document.getElementById('ul');
+
+    lastBtn.addEventListener('click', function () {
+        let randomFriend = Math.floor(Math.random() * friends.length)
+        
+            let li = document.createElement('li');
+            let friendNames = document.createTextNode(friends[randomFriend])
+            li.appendChild(friendNames)
+            ul.appendChild(li);
+        
+    });
+
+    // End of DOM Content Loaded tag
+});
--- JQuery/app.js
+++ JQuery/app.js
@@ -1,1 +1,52 @@
-ECHO is on.
+let $ul = $('#ul')
+$listBtn.click(function()   {
+    let $randomFriend = Math.floor(Math.random() * $friends.length)
+    for (let i = 0; i < 1; i++) {
+
+        let $friendNames = ($friends[$randomFriend])
+        let $li = $(`<li>${$friendNames} </li>`);
+        $ul.append($li);
+    }
+
+})
+
+$(document).ready(function (){
+
+    let $button = $('<button>Nice Button</Button>');
+    $('body').append($button)
+    $button.click(function()    {
+        alert('Hiya my dood')
+    });
+    
+    let htmlButton = $('#htmlButton')
+    htmlButton.click(function() {
+        alert($('#textBox').val())
+    });
+    
+    //change background color
+    let $colorDiv = $('#div1')
+    $colorDiv.mouseenter(function() {
+       $(this).css('backgroundColor', 'red')
+    });
+    $colorDiv.mouseleave(function() {
+        $(this).css('backgroundColor', 'green')
+    
+    });
+    
+    
+        // Color change func
+        let $p = $('<p>Paragraphing out</p>')
+        $('body').append($p)
+            
+        $p.click(function() {
+            let r = Math.floor((Math.random() * 255) + 1);
+            let g = Math.floor((Math.random() * 255) + 1);
+            let b = Math.floor((Math.random() * 255) + 1);
+            $(this).css('color',  "rgb(" + r + ", " + g + ", " + b + ")")
+        });
+    
+    
+    
+
+    });
+
